edb074bcfff0c8903f3604d4d99fdb8c13932503
OldBoyInHDU <973157930@qq.com>
2022-10-10 15:28:54 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import axios from 'axios'
+import service from '@/utils/request'
+import ViewUI from 'view-design'
+import App from './App'
+import store from './store'
+import router from './router'
+import 'view-design/dist/styles/iview.css'
+import './permission'
+
+Vue.config.productionTip = false
+Vue.use(ViewUI)
+
+Vue.prototype.$axios = axios
+Vue.prototype.service = service
+axios.defaults.baseURL = 'http://localhost:8088/achieve'
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+})
